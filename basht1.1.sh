b751d3d75d605348a261d2eba28847da70bf86e6
arun <smartboyarun560@gmail.com>
2017-09-09 21:38:29 +0530

    Add files via upload

---
+++ basht1.1.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+clear
+uuid=$(cat /proc/sys/kernel/random/uuid);
+for f in /home/sathish/new/ftp_user218/*;
+do 
+find $f -type d -name "*[0-9].*""[0-9].*""[0-9].*""[0-9]*" -mmin +300 -exec mv "{}" ${f/${f:30:11}/$uuid} \;
+done 
+echo " the process done"
+
+
+
+  
---
+++ curl_Task.bat
@@ -0,0 +1,4 @@
+@echo off
+rem For each file in your folder
+for  %%i in (".\*") do (curl -T %%i ftp://localhost --user sathish:smart@123)
+pause
